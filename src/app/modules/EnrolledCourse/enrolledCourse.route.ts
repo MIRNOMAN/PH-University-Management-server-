8750d896a01cdee537861906832adcb7ed676e75
MIRNOMAN <abdullahalnoman1509@gmail.com>
2024-10-05 22:30:01 +0600

    update enrolled course

---
+++ src/app/modules/EnrolledCourse/enrolledCourse.route.ts
@@ -0,0 +1,34 @@
+import express from 'express';
+import { EnrolledCourseControllers } from './enrolledCourse.controller';
+import { EnrolledCourseValidations } from './enrolledCourse.validaton';
+import auth from '../../../middlewares/auth';
+import { USER_ROLE } from '../user/user.constant';
+import { validateRequest } from '../../../middlewares/validateRequest';
+
+const router = express.Router();
+
+router.post(
+  '/create-enrolled-course',
+  auth(USER_ROLE.student),
+  validateRequest(
+    EnrolledCourseValidations.createEnrolledCourseValidationZodSchema,
+  ),
+  EnrolledCourseControllers.createEnrolledCourse,
+);
+
+router.get(
+  '/my-enrolled-courses',
+  auth(USER_ROLE.student),
+  EnrolledCourseControllers.getMyEnrolledCourses,
+);
+
+router.patch(
+  '/update-enrolled-course-marks',
+  auth(USER_ROLE.superAdmin, USER_ROLE.admin, USER_ROLE.faculty),
+  validateRequest(
+    EnrolledCourseValidations.updateEnrolledCourseMarksValidationZodSchema,
+  ),
+  EnrolledCourseControllers.updateEnrolledCourseMarks,
+);
+
+export const EnrolledCourseRoutes = router;
